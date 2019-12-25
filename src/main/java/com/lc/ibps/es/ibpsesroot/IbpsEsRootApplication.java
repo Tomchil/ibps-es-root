461134455d153e6f13d9143a23cd499efcde2f93
tomch <tomchil@qq.com>
2019-12-25 14:10:32 +0800

    Initial commit

---
+++ src/main/java/com/lc/ibps/es/ibpsesroot/IbpsEsRootApplication.java
@@ -0,0 +1,13 @@
+package com.lc.ibps.es.ibpsesroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbpsEsRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbpsEsRootApplication.class, args);
+    }
+
+}
